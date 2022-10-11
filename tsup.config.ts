1d17040d6a2663547d3392cdeed8bf1a4451cce7
P-Chan <hello@0x50.io>
2022-10-11 19:05:46 +0900

    chore: use tsup instead of tsc

---
+++ tsup.config.ts
@@ -0,0 +1,10 @@
+import { defineConfig } from 'tsup'
+
+export default defineConfig((options) => ({
+  clean: true,
+  dts: true,
+  entry: ['./src/index.ts'],
+  format: ['cjs', 'esm'],
+  minify: !options.watch,
+  splitting: false,
+}))
